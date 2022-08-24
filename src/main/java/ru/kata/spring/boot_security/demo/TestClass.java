f0776bfa377faf374b7a7efbe848dc00e7ecffbf
Artem311k <101522028+Artem311k@users.noreply.github.com>
2022-08-24 20:28:45 +0300

    Update TestClass.java
    uncomment

--- src/main/java/ru/kata/spring/boot_security/demo/TestClass.java
+++ src/main/java/ru/kata/spring/boot_security/demo/TestClass.java
@@ -25,29 +25,29 @@ public class TestClass {
 
     @PostConstruct
     public void init() {
-//        User admin = new User("Artem", "Kuzmin", "admin@admin", 31); // создаём Админа, логин - admin@admin
-//        admin.setPassword("100"); //пароль админа - "100"
-//        Role adminRole = new Role("ROLE_ADMIN"); // создаём роль АДМИН
-//        adminService.addUser(admin); // добваляем админа в базу
-//        roleService.addRole(adminRole); // добавляем роль в базу
-//        admin.addRole(adminRole); // назначем роль Админу
-//        adminService.updateUser(admin); // сохраняем изменения в админе
-//
-//        User user1 = new User("Ivan", "Petrov", "petrov@petrov", 24); // создаём Юзера, логин - petrov@petrov
-//        user1.setPassword("100"); //пароль юзера - "100"
-//        Role userRole = new Role("ROLE_USER"); // создаём роль ЮЗЕР
-//        adminService.addUser(user1); // добавляем юзера в базу
-//        roleService.addRole(userRole); // добавляем роль в базу
-//        user1.addRole(userRole); // назначаем роль юзеру
-//        adminService.updateUser(user1); // сохраняем изменения в юзере
-//
-//
-//        User comboUser = new User("Sam", "Fisher", "fisher@fisher", 54); // создаём юзера с двумя ролями, логин - fisher@fisher
-//        comboUser.setPassword("100"); // пароль юзера - "100"
-//        adminService.addUser(comboUser); // добавляем юзера в базу
-//        comboUser.addRole(adminRole); // назначем роль админа
-//        comboUser.addRole(userRole); // назначем роль юзера
-//        adminService.updateUser(comboUser); // сохраняем изменения в юзере
+        User admin = new User("Artem", "Kuzmin", "admin@admin", 31); // создаём Админа, логин - admin@admin
+        admin.setPassword("100"); //пароль админа - "100"
+        Role adminRole = new Role("ROLE_ADMIN"); // создаём роль АДМИН
+        adminService.addUser(admin); // добваляем админа в базу
+        roleService.addRole(adminRole); // добавляем роль в базу
+        admin.addRole(adminRole); // назначем роль Админу
+        adminService.updateUser(admin); // сохраняем изменения в админе
+
+        User user1 = new User("Ivan", "Petrov", "petrov@petrov", 24); // создаём Юзера, логин - petrov@petrov
+        user1.setPassword("100"); //пароль юзера - "100"
+        Role userRole = new Role("ROLE_USER"); // создаём роль ЮЗЕР
+        adminService.addUser(user1); // добавляем юзера в базу
+        roleService.addRole(userRole); // добавляем роль в базу
+        user1.addRole(userRole); // назначаем роль юзеру
+        adminService.updateUser(user1); // сохраняем изменения в юзере
+
+
+        User comboUser = new User("Sam", "Fisher", "fisher@fisher", 54); // создаём юзера с двумя ролями, логин - fisher@fisher
+        comboUser.setPassword("100"); // пароль юзера - "100"
+        adminService.addUser(comboUser); // добавляем юзера в базу
+        comboUser.addRole(adminRole); // назначем роль админа
+        comboUser.addRole(userRole); // назначем роль юзера
+        adminService.updateUser(comboUser); // сохраняем изменения в юзере
 
 
 
